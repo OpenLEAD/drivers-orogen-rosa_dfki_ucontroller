0b660b2242c4da0d16082a30673fc187b3faf6c9
elael <elael2@gmail.com>
2015-02-06 01:30:41 -0200

    initial commit by rock-create-orogen

---
+++ rosa_dfki_ucontrollerTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef rosa_dfki_ucontroller_TYPES_HPP
+#define rosa_dfki_ucontroller_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace rosa_dfki_ucontroller {
+}
+
+#endif
+
